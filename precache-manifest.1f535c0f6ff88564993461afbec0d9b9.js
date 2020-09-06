65b5fa3c11ae73a4f06350d471b2f806e2a9e9c5
ariful-sagor <ariful.islam200069@gmail.com>
2020-09-07 01:13:25 +0600

    Updates

---
+++ precache-manifest.1f535c0f6ff88564993461afbec0d9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4bfdce9f056650a2f23fb4015d5398",
+    "url": "/my-posts/index.html"
+  },
+  {
+    "revision": "1a2cc0b1679fc8ea6d92",
+    "url": "/my-posts/static/css/main.c42dfed8.chunk.css"
+  },
+  {
+    "revision": "f22a1f7d8a5fc37ce016",
+    "url": "/my-posts/static/js/2.5d2d8bd4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-posts/static/js/2.5d2d8bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2cc0b1679fc8ea6d92",
+    "url": "/my-posts/static/js/main.8e8d787b.chunk.js"
+  },
+  {
+    "revision": "2510aa69c1a2845d65f2",
+    "url": "/my-posts/static/js/runtime-main.9ec00201.js"
+  }
+]);
